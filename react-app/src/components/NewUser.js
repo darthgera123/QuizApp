14bca75c65e320c44f2a49fd6ae14e7be6f18292
Sahil Singh <sahilsinghmca@gmail.com>
2019-03-31 03:35:38 +0530

    Improved Login and SignUp page UI

--- react-app/src/components/NewUser.js
+++ react-app/src/components/NewUser.js
@@ -14,18 +14,17 @@ class App extends Component {
             <nav className="navbar navbar-default">
               <div className="container-fluid">
                 <div className="navbar-header">
-                  <Link className="navbar-brand" to={'/'}>React App</Link>
+                  <Link className="navbar-brand" to={'/'}>Quiz App</Link>
                 
                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNav">
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
                 </button>
                 </div>
                 <div className="collapse-navbar-collapse" id="myNav">
                 <ul className="nav navbar-nav">
                   <li className="active"><Link to={'/'}>Home</Link></li>
-                  
                   <li><Link to={'/NewPerson'}>Sign-Up</Link></li>
                   <li><Link to={'/Login'}>Login</Link></li>
                 </ul>
@@ -33,10 +32,9 @@ class App extends Component {
               </div>
             </nav>
             <Switch>
-                 <Route exact path='/' component={Home} />
-                 <Route exact path='/NewPerson' component={NewPerson} />
-                 <Route exact path='/Login' component={Login} />
-                 
+              <Route exact path='/' component={Home} />
+              <Route exact path='/NewPerson' component={NewPerson} />
+              <Route exact path='/Login' component={Login} />        
             </Switch>
           </div>
         </Router>
